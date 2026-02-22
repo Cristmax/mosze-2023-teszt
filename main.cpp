4dd742870caeb1c04d0ca637fb1f5f29b10fedfc
Cristmax <molnar.roland090@gmail.com>
2026-02-22 18:27:35 +0100

    Hibák kommentelése a main.cpp fájlban

--- main.cpp
+++ main.cpp
@@ -4,23 +4,43 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+            //NELEMENTS helytelen név, helyesen N_ELEMENTS
+    int* b = new int[N_ELEMENTS];
+
+            //hiányzó pontosvesszõ
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+
+            //hibás for ciklus fejléc
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+
+            //hibás ciklusfeltétel (i)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i]
+            // hiányzó kiírási rész és pontosvesszõ
+        std::cout << "Ertek: " << b[i] << std::endl;
     }
+
+    std::cout << "Atlag szamitasa: " << std::endl;
+
+            //nincs inicializálva
+    int atlag = 0;
+
+            //vesszõ a feltételben
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+            //hiányzó pontosvesszõ
+        atlag += b[i];
+    }
+
     atlag /= N_ELEMENTS;
+
     std::cout << "Atlag: " << atlag << std::endl;
+
+         //hiányzó delete[]
+    delete[] b;
+
     return 0;
 }
